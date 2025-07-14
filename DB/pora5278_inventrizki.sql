966ccaea6cfff3543902d7372b67ac6e83e2733a
korezsyndicate5 <korezsyndicate5@gmail.com>
2025-07-14 20:26:00 +0700

    revoke db

--- DB/pora5278_inventrizki.sql
+++ DB/pora5278_inventrizki.sql
@@ -23,9 +23,12 @@ CREATE TABLE IF NOT EXISTS `barang_keluar` (
   `nama_barang` varchar(50) NOT NULL DEFAULT '0',
   `jumlah` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dumping data for table pora5278_inventrizki.barang_keluar: ~0 rows (approximately)
+-- Dumping data for table pora5278_inventrizki.barang_keluar: ~2 rows (approximately)
+INSERT INTO `barang_keluar` (`id`, `id_transaksi`, `tanggal`, `kode_barang`, `nama_barang`, `jumlah`) VALUES
+	(2, 'TRK-072500', '2025-07-07', 'BAR-072500', 'Ayam', 50),
+	(3, 'TRK-072500', '2026-10-07', 'BAR-072500', 'Ayam', 10);
 
 -- Dumping structure for table pora5278_inventrizki.barang_masuk
 CREATE TABLE IF NOT EXISTS `barang_masuk` (
@@ -37,43 +40,39 @@ CREATE TABLE IF NOT EXISTS `barang_masuk` (
   `pengirim` varchar(20) NOT NULL,
   `jumlah` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dumping data for table pora5278_inventrizki.barang_masuk: ~4 rows (approximately)
+-- Dumping data for table pora5278_inventrizki.barang_masuk: ~1 rows (approximately)
 INSERT INTO `barang_masuk` (`id`, `id_transaksi`, `tanggal`, `kode_barang`, `nama_barang`, `pengirim`, `jumlah`) VALUES
-	(1, 'TRM-072500', '2025-07-07', 'BAR-072500', 'Ayam', 'PT Gading Murni', 100),
-	(2, 'TRM-072500', '2025-08-07', 'BAR-072500', 'Ayam', 'PT Gading Murni', 150),
-	(3, 'TRM-072500', '2025-09-07', 'BAR-072500', 'Ayam', 'PT Gading Murni', 250),
-	(4, 'TRM-072500', '2025-10-07', 'BAR-072500', 'Ayam', 'PT Gading Murni', 120);
+	(14, 'TRM-072500', '2025-07-07', 'BAR-072500', 'Ayam', 'PT Gading Murni', 200),
+	(15, 'TRM-072500', '2026-03-07', 'BAR-072500', 'Ayam', 'PT Gading Murni', 100);
 
 -- Dumping structure for table pora5278_inventrizki.gudang
 CREATE TABLE IF NOT EXISTS `gudang` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `kode_barang` varchar(10) NOT NULL,
-  `nama_barang` varchar(50) NOT NULL,
-  `jenis_barang` varchar(10) NOT NULL,
-  `jumlah` int(11) NOT NULL DEFAULT 0,
+  `kode_barang` varchar(100) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jenis_barang` varchar(100) NOT NULL,
+  `jumlah` int(100) NOT NULL DEFAULT 0,
   `satuan` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- Dumping data for table pora5278_inventrizki.gudang: ~3 rows (approximately)
+-- Dumping data for table pora5278_inventrizki.gudang: ~0 rows (approximately)
 INSERT INTO `gudang` (`id`, `kode_barang`, `nama_barang`, `jenis_barang`, `jumlah`, `satuan`) VALUES
-	(18, 'BAR-072500', 'Ayam', 'Barang Bas', 620, 'PCS'),
-	(19, 'BAR-072500', 'Tepung', 'Barang Ker', 0, 'PCS'),
-	(20, 'BAR-072500', 'plastik', 'Barang Ker', 0, 'PCS');
+	(3, 'BAR-0725001', 'Ayam', 'Barang Basah', 240, 'PCS');
 
 -- Dumping structure for table pora5278_inventrizki.jenis_barang
 CREATE TABLE IF NOT EXISTS `jenis_barang` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `jenis_barang` varchar(10) NOT NULL,
+  `jenis_barang` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- Dumping data for table pora5278_inventrizki.jenis_barang: ~2 rows (approximately)
 INSERT INTO `jenis_barang` (`id`, `jenis_barang`) VALUES
-	(1, 'Barang Bas'),
-	(2, 'Barang Ker');
+	(1, 'Barang Basah'),
+	(2, 'Barang Kering');
 
 -- Dumping structure for table pora5278_inventrizki.satuan
 CREATE TABLE IF NOT EXISTS `satuan` (
@@ -123,26 +122,6 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`id`, `nik`, `nama`, `alamat`, `telepon`, `username`, `password`, `level`, `foto`) VALUES
 	(1, '1900120001', 'Rizki', '', '0811228890', 'rizki', '$2y$10$nUIglaVvTwg/hm75F2mwAO6qrI7J1IOobNLdSESEdJJ6iRIblG/vK', 'admin', 'Desktop - 48.png'),
 	(2, '1900126005', 'aryamurti', '', '085546982011', 'arya', '5882985c8b1e2dce2763072d56a1d6e5', 'petugas', 'Desktop - 48.png');
-
--- Dumping structure for trigger pora5278_inventrizki.barang_keluar
-SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
-DELIMITER //
-CREATE TRIGGER `barang_keluar` AFTER INSERT ON `barang_keluar` FOR EACH ROW BEGIN
-	UPDATE gudang SET jumlah = jumlah-new.jumlah
-    WHERE kode_barang=new.kode_barang;
-    END//
-DELIMITER ;
-SET SQL_MODE=@OLDTMP_SQL_MODE;
-
--- Dumping structure for trigger pora5278_inventrizki.barang_masuk
-SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO';
-DELIMITER //
-CREATE TRIGGER `barang_masuk` AFTER INSERT ON `barang_masuk` FOR EACH ROW BEGIN
-	UPDATE gudang SET jumlah = jumlah+new.jumlah
-    WHERE kode_barang=new.kode_barang;
-    END//
-DELIMITER ;
-SET SQL_MODE=@OLDTMP_SQL_MODE;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
