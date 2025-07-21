247ce0dd08875f8997116c37e788300bad98a31d
rezafahmialviandy <fahmi@cakrawala.ac.id>
2025-07-21 23:14:58 +0700

    revoke code

--- page/laporan/export_laporan_requestbarang_pdf.php
+++ page/laporan/export_laporan_requestbarang_pdf.php
@@ -18,9 +18,9 @@ class RequestBarangPDF extends FPDF
             $this->report_title = "Laporan Request Barang Tahun $thn";
         } else {
             $month_names = [
-                '01' => 'Januari', '02' => 'Februari', '03' => 'Maret',
-                '04' => 'April', '05' => 'Mei', '06' => 'Juni',
-                '07' => 'Juli', '08' => 'Agustus', '09' => 'September',
+                '1' => 'Januari', '2' => 'Februari', '3' => 'Maret',
+                '4' => 'April', '5' => 'Mei', '6' => 'Juni',
+                '7' => 'Juli', '8' => 'Agustus', '9' => 'September',
                 '10' => 'Oktober', '11' => 'November', '12' => 'Desember'
             ];
             $month_name = $month_names[$bln] ?? "Bulan $bln";
